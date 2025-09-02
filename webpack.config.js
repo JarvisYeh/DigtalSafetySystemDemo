9d443e692a1228914bf5ba0980d607b9beb1affd
OneDay AI 代码助手 <oneday_bot@alibaba-inc.com>
2025-09-03 01:17:20 +0800

    更改输出目录名

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   mode: 'development',
   entry: './src/index.jsx',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'build'),
     filename: 'bundle.js',
     clean: true
   },
